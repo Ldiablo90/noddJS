57572129b21d5cf5018a1f3331cdc464d81128e7
Ldiablo90 <ldiablo90@gmail.com>
2021-12-07 16:35:10 +0900

    resetfile

--- node.js-mysql-1/lib/template.js
+++ node.js-mysql-1/lib/template.js
@@ -1,34 +1,28 @@
 module.exports = {
-  HTML: function (title, body, list, controll = '') {
-    var resualt =
-      `
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
-            </head>
-            <body>
-                <h1><a href="/">Page Title</a></h1>
-                ${list}
-                <a href="/create">create</a>${controll}
-                <div>
-                    <h2>${title}</h2>
-                    ${body}
-                </div>
-            </body>
-        </html>
-        `;
-    return resualt;
-  },
-  list: function (filelist) {
-    var list = '<ol>';
-    filelist.array.forEach(file => {
-      list += `<li><a href="/?id=${file}">${file}</a></li>`;
-    });
-    list = list + '</ol>';
+  HTML:function(title, list, body, control){
+    return `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${control}
+      ${body}
+    </body>
+    </html>
+    `;
+  },list:function(filelist){
+    var list = '<ul>';
+    var i = 0;
+    while(i < filelist.length){
+      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+      i = i + 1;
+    }
+    list = list+'</ul>';
     return list;
   }
 }
