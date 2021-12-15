5c26ba8522764e38d9b93f26009a901b67d770cd
Ldiablo90 <ldiablo90@gmail.com>
2021-12-15 22:02:58 +0900

    흐아... 1일1커밋...
    https://lian-is-clone.tistory.com/15

--- FullStackMERNProject/client/src/App.js
+++ FullStackMERNProject/client/src/App.js
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
-import Posts from './components/Posts/Posts.js'
-import Form from './components/Form/Form.js'
+import { useDispatch } from 'react-redux';
 
-import memories from './components/images/memories.jpg'
+import { getPosts } from './actions/posts';
+
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import memories from './images/memories.jpg'
+import useStyles from './style'
+
 
 const App = ()=>{
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    },[dispatch]);
+
     return (
         <Container maxWidth='lg'>
-            <AppBar position='static' color='inherit'>
-                <Typography variant='h2' align=' center'>Menries</Typography>
-                <img src={memories} alt='memories' heigth="60"/>
+            <AppBar className={classes.appBar} position='static' color='inherit'>
+                <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+                <img className={classes.image} src={memories} alt='memories' heigth="60"/>
             </AppBar>
             <Grow in>
                 <Container>
                     <Grid Container justifyContent='space-between' alignItems='stretch' spacing={3}>
                         <Grid item xs={12} sm={7}>
-                            <Posts></Posts>
+                            <Posts/>
                         </Grid>
                         <Grid item xs={12} sm={4}>
-                            <Form></Form>
+                            <Form/>
                         </Grid>
                     </Grid>
                 </Container>
--- FullStackMERNProject/client/src/components/Posts/Posts.js
+++ FullStackMERNProject/client/src/components/Posts/Posts.js
@@ -1,7 +1,14 @@
 import React from "react";
-import Post from './Post/Post.js'
+import { useSelector } from "react-redux";
+
+import Post from './Post/Post'
+import useStyles from './style'
 
 const Posts = ()=> {
+    const posts = useSelector((state) => state.posts);
+    // const classes = useStyles();
+
+    console.log(posts);
     return (
         <>
             <h1>Posts</h1>
